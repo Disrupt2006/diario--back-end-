0d0abe9c751b1c5d92f7caa78ff306b78b1e8194
Thiago Orué <89083464+Disrupt2006@users.noreply.github.com>
2022-06-24 12:04:46 -0300

    avances generales

--- js/diario.js
+++ js/diario.js
@@ -1,4 +1,5 @@
-//cuando termina de bajar todo comienza a leer todo lo de adentro
+//cuando termina de bajar todo (archivo index que tiene dentro el link al css y js) 
+//comienza a leer todo lo de adentro
 $(document).ready(function()
 {
 
@@ -11,23 +12,33 @@ $(document).ready(function()
 		//cambiar icono de mouse (llamar por clase)
 		$(".boton").css("cursor", "pointer");
 
-		//si #u esta vacia
+		//si #u y #p estan vacias 
 		//.val --> levanta valor de elemento
 		if ($("#u").val()== '' && $("#p").val() == '')
 		{
 			//meter en mensaje desde html y desocultarlo
 			$("#divt").html ("Debe ingresar Usuario y Contraseña");
 			$("#divt").show();
+
+			//poner color de rojo
+			$("#1").css("color","red");
+			$("#2").css("color","red");
+
 		}
 		else if ($('#u').val() != '' && $('#p').val()=='')
 		{
 			$("#divt").html("Debe ingresar Contraseña");
 			$("#divt").show();
+
+			$("#2").css("color", "red");
 		}
 		else if ($('#u').val() == '' && $('#p').val() != '')
 		{
+
 			$("#divt").html("Debe ingresar Usuario");
 			$("#divt").show();	
+
+			$("#2").css("color", "red");
 		}
 		
 		else
@@ -40,3 +51,8 @@ $(document).ready(function()
 
 
 });
+
+//me falta:
+//cambiar el color de los labels cuando falta info
+//poner highlight a textboxes cuando apoyas mouse
+//agarrar nombre y apellido de json con .ajax
--- php/validar.php
+++ php/validar.php
@@ -4,7 +4,7 @@ $con = new mysqli ("localhost", "root", "rootroot");
 #Seleccionar base de datos
 mysqli_select_db($con, "diario");
 
-#Prevenir error de conexion (si hay error que mate el proceso)
+#Prevenir error de conexion (si hay error, que mate el proceso)
 if ($con->connect_error)
 {	
 	#Mata proceso
@@ -17,7 +17,7 @@ $sql="SELECT id_usu,nombre,apellido FROM usuarios WHERE usuario='" . $_REQUEST['
 #Resultado que devulve la base de datos
 $res = $con->query($sql);
 
-$i = 0;  #no vimos que es
+$i = 0;  #representa a las filas, si no existen sera 0
 
 #Devuelve cantidad de filas que tiene resultado (si existe 1,sino 0)
 if ($res->num_rows > 0)
@@ -43,11 +43,13 @@ else
 	$data ['result'] = '';
 }
 
-#se encoda a json
+#se encoda a json y se retorna
 echo json_encode($data);
 
 #cerrar conexion
 $con->close();
+
+#localhost/diario/php/validar.php?usu=yo&PASS=123
 
 
 ?>
